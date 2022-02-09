dc2764cc6f86b993c0f8b6eef303246d27e28676
Brajesh Singh <40537685+thakurbrajeshsingh@users.noreply.github.com>
2022-02-09 15:02:28 +0530

    Count Down Added

--- src/Container/Otp/Otp.js
+++ src/Container/Otp/Otp.js
@@ -1,18 +1,28 @@
-import React, { useState ,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
-import { CustomOtp, CustomLabel } from '../../component'
+import { Caption, Title, Headline, Paragraph } from 'react-native-paper';
+import { CustomOtp, CustomLabel, CustomButton } from '../../component'
+import ONBOARDING from '../../Config/Constant/Constant'
 
-
-const Otp = () => {
+const Otp = ({ navigation }) => {
 
   // Timer 
-  const [counter, setCounter] = React.useState(59);
-  React.useEffect(() => {
-      const timer =
-      counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
-      return () => clearInterval(timer);
-  }, [counter]);
+  const [counter, setCounter] = useState(30);
+  const [resend, setResend] = useState("");
 
+  const handleResend = () => {
+    setCounter(30)
+    setResend("")
+  }
+
+  useEffect(() => {
+    const timer = counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
+    if (timer == 0) {
+      setCounter("")
+      setResend("Resend")
+    }
+    return () => clearInterval(timer);
+  }, [counter]);
 
 
 
@@ -22,11 +32,13 @@ const Otp = () => {
     <ImageBackground source={image} style={styles.image} blurRadius={5}>
       <View style={{ marginTop: "auto", marginBottom: "auto", padding: 10 }}>
         <CustomLabel label='Have You Received a Verification Code ?' />
-        <Text style={{ padding: 10,color:"#ffffff",fontWeight:"bold" }}>
-          we are automatically detectinga SMS sent to your mobile number ***8646
-        </Text>
+        <Paragraph style={{ padding: 10, color: "#ffffff", fontWeight: "bold" }}>
+          We are automatically detectinga SMS sent to your mobile number ***8646
+        </Paragraph>
         <CustomOtp pinCount={6} />
-       <Text>Resending Otp in <Text style={{fontSize:15,fontWeight:"bold",color:"green"}}>{counter}</Text> </Text> 
+        <Title >Resending Otp in <Headline style={{ fontWeight: "bold", color: "green" }}>{counter}</Headline>
+        </Title>
+        <Title style={{ fontSize: 15, color: "yellow" }} onPress={handleResend}  >{resend}</Title>
       </View>
     </ImageBackground>
   )
